6e48473e7249a7e568603c6ea4e2ccf856c3e88e
Pavel Feldman <feldmanpk@gmail.com>
2024-05-02 20:16:15 +0300

    CW #18 difference between temporal's

--- src/telran/time/test/DateTimeTest.java
+++ src/telran/time/test/DateTimeTest.java
@@ -2,8 +2,9 @@ package telran.time.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.time.LocalDate;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Locale;
 
 import org.junit.jupiter.api.Test;
@@ -26,6 +27,16 @@ class DateTimeTest {
 		System.out.printf("birthdate of ASP in ... is %s\n", 
 				birthDateAS.format(DateTimeFormatter.ofPattern("MMMM,d.yyy",
 						Locale.forLanguageTag("he"))));
+		
+		LocalDate d1 = LocalDate.parse("1990-01-30");
+		LocalDate d2 = LocalDate.parse("2000-10-20");
+		ChronoUnit unit1 = ChronoUnit.MONTHS;
+		System.out.printf("difference between %s and %s in %s is %d\n", d1, d2, unit1, unit1.between(d1, d2));
+
+		LocalDateTime d3 = LocalDateTime.parse("1990-01-30T00:00:00");
+		LocalDateTime d4 = LocalDateTime.parse("2000-10-20T00:00:00");
+		ChronoUnit unit2 = ChronoUnit.SECONDS;
+		System.out.printf("difference between %s and %s in %s is %d\n", d3, d4, unit2, unit2.between(d4, d3));
 		
 	}
 
